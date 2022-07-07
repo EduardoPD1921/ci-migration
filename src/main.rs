374eb530e10c0e01a216d2edeb6be6897cd80ea9
Eduardo Gomes Heleno <eduardo.gheleno@gmail.com>
2022-07-07 17:04:24 -0300

    ✨ format time as migration title

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::env;
+use chrono::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = env::args().skip(1);
+    let migration_name = args.next();
+
+    match migration_name {
+        Some(_s) => {
+            let dt = Local::now();
+
+            let hour = format_time(dt.hour());
+            let minute = format_time(dt.minute());
+
+            println!("{}{}", hour, minute);
+        }
+        None => {
+            println!("Insert your migration name.");
+        }
+    }
+}
+
+
+fn format_time(time: u32) -> String {
+    let formatted_time = if time < 10 { format!("0{}", time.to_string()) } else { time.to_string() };
+    formatted_time
 }
